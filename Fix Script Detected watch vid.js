f5a5fd2ce876fb5912cf2688151c27984cb57bc9
techycoder <51840467+techycoder@users.noreply.github.com>
2019-06-17 17:45:23 -0600

    Update Fix Script Detected watch vid.js

--- Fix Script Detected watch vid.js
+++ Fix Script Detected watch vid.js
@@ -6,13 +6,11 @@
 // @author       TechyCoder
 // @match        https://www.10bestseo.com/url-opener/
 // @grant        none
-// @logo         https://yt3.ggpht.com/a/AGF-l79OiM4TByerDjP823w20yI0IF4dM12e4vuE-w=s176-c-k-c0x00ffffff-no-rj-mo
 // ==/UserScript==
 
-
-
-document.write( '<html lang=\"en\"><head>\n' );
-document.write( '<script src=\"https://www.googletagservices.com/activeview/js/current/osd.js?cb=%2Fr20100101\"></script><script src=\"https://pagead2.googlesyndication.com/pub-config/r20160913/ca-pub-1223664168487159.js\"></script><script type=\"text/javascript\" async=\"\" src=\"https://www.google-analytics.com/analytics.js\"></script><script async=\"\" src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>\n' );
+document.write( '<html lang=\"en\">\n' );
+document.write( '<head>\n' );
+document.write( '<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>\n' );
 document.write( '<script>\n' );
 document.write( '         (adsbygoogle = window.adsbygoogle || []).push({\n' );
 document.write( '              google_ad_client: \"ca-pub-1223664168487159\",\n' );
@@ -34,19 +32,22 @@ document.write( '<link href=\"/url-opener/vendor/magnific-popup/magnific-popup.c
 document.write( '\n' );
 document.write( '<link href=\"/url-opener/css/freelancer.css\" rel=\"stylesheet\">\n' );
 document.write( '\n' );
-document.write( '<script async=\"\" src=\"https://www.googletagmanager.com/gtag/js?id=UA-31480580-2\"></script>\n' );
+document.write( '<script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-31480580-2\"></script>\n' );
 document.write( '<script>\n' );
 document.write( '      window.dataLayer = window.dataLayer || [];\n' );
 document.write( '      function gtag(){dataLayer.push(arguments);}\n' );
 document.write( '      gtag(\'js\', new Date());\n' );
 document.write( '    \n' );
 document.write( '      gtag(\'config\', \'UA-31480580-2\');\n' );
-document.write( '    </script><link rel=\"preload\" href=\"https://adservice.google.ca/adsid/integrator.js?domain=www.10bestseo.com\" as=\"script\"><script type=\"text/javascript\" src=\"https://adservice.google.ca/adsid/integrator.js?domain=www.10bestseo.com\"></script><link rel=\"preload\" href=\"https://adservice.google.com/adsid/integrator.js?domain=www.10bestseo.com\" as=\"script\"><script type=\"text/javascript\" src=\"https://adservice.google.com/adsid/integrator.js?domain=www.10bestseo.com\"></script><link rel=\"preload\" href=\"https://pagead2.googlesyndication.com/pagead/js/r20190612/r20190131/show_ads_impl.js\" as=\"script\">\n' );
+document.write( '    </script>\n' );
 document.write( '<script type=\"text/javascript\">\n' );
 document.write( '      function openall() {\n' );
-document.write( '  }\n' );
+document.write( '  window.open(\"http://krunker.io\", \"1560815412c1\");\n' );
+document.write( 'window.open(\"http://krunker.io\", \"1560815412c2\");\n' );
+document.write( 'window.open(\"http://krunker.io\", \"1560815412c3\");\n' );
+document.write( '}\n' );
 document.write( '    </script>\n' );
-document.write( '</head><iframe id=\"google_shimpl\" style=\"display: none;\"></iframe>\n' );
+document.write( '</head>\n' );
 document.write( '<body id=\"page-top\">\n' );
 document.write( '\n' );
 document.write( '<nav class=\"navbar navbar-expand-lg bg-primary text-uppercase\" id=\"mainNav\">\n' );
@@ -76,15 +77,28 @@ document.write( '<header class=\"masthead bg-primary text-white text-center\">\n
 document.write( '<div class=\"container\">\n' );
 document.write( '<h1 style=\"font-family: \'Barlow Semi Condensed\', sans-serif;\">URL Opener</h1>\n' );
 document.write( '<p style=\"font-weight: 100; font-size: .8em;\">Open a whole list of URLs at the same time! Paste URLs into the field below and press Submit. Press Open All (which will appear) to pop up all the websites at once, or open each URL individually. We recommend setting your browser to open new windows in tabs (here\'s how). Note: Opening too many at once can slow your computer.</p>\n' );
-document.write( '<script async=\"\" src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>\n' );
+document.write( '<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>\n' );
 document.write( '\n' );
-document.write( '<ins class=\"adsbygoogle\" style=\"display: block; height: 90px;\" data-ad-client=\"ca-pub-1223664168487159\" data-ad-slot=\"6520099878\" data-ad-format=\"auto\" data-adsbygoogle-status=\"done\"><ins id=\"aswift_1_expand\" style=\"display:inline-table;border:none;height:90px;margin:0;padding:0;position:relative;visibility:visible;width:1110px;background-color:transparent;\"><ins id=\"aswift_1_anchor\" style=\"display:block;border:none;height:90px;margin:0;padding:0;position:relative;visibility:visible;width:1110px;background-color:transparent;\"><iframe width=\"1110\" height=\"90\" frameborder=\"0\" marginwidth=\"0\" marginheight=\"0\" vspace=\"0\" hspace=\"0\" allowtransparency=\"true\" scrolling=\"no\" allowfullscreen=\"true\" onload=\"var i=this.id,s=window.google_iframe_oncopy,H=s&&s.handlers,h=H&&H[i],w=this.contentWindow,d;try{d=w.document}catch(e){}if(h&&d&&(!d.body||!d.body.firstChild)){if(h.call){setTimeout(h,0)}else if(h.match){try{h=s.upd(h,i)}catch(e){}w.location.replace(h)}}\" id=\"aswift_1\" name=\"aswift_1\" style=\"left:0;position:absolute;top:0;border:0px;width:1110px;height:90px;\"></iframe></ins></ins></ins>\n' );
+document.write( '<ins class=\"adsbygoogle\" style=\"display:block\" data-ad-client=\"ca-pub-1223664168487159\" data-ad-slot=\"6520099878\" data-ad-format=\"auto\"></ins>\n' );
 document.write( '<script>\n' );
 document.write( '(adsbygoogle = window.adsbygoogle || []).push({});\n' );
 document.write( '</script>\n' );
 document.write( '<br>\n' );
 document.write( '<form action=\"/url-opener/\" method=\"post\">\n' );
 document.write( '<div class=\"form-group\">\n' );
-document.write( '<textarea class=\"form-control\" name=\"id\" rows=\"10\">' );
-
+document.write( '<div id=\"results\">\n' );
+document.write( '<button onclick=\"openall(); return false;\" class=\"btn btn-warning btn-lg btn-block\">Open All</button>\n' );
+document.write( '<button class=\"btn-danger btn btn-lg btn-block\" onclick=\"return false;\" data-toggle=\"modal\" data-target=\"#exampleModalCenter\">Unable to open all sites? Click here!</button>\n' );
+document.write( '<div class=\"list-group\">\n' );
+document.write( '<a class=\"list-group-item list-group-item-action\" href=\"http://krunker.io\" target=\"1560815412c1\">krunker.io</a>\n' );
+document.write( '<a class=\"list-group-item list-group-item-action\" href=\"http://krunker.io\" target=\"1560815412c2\">krunker.io</a>\n' );
+document.write( '</div>\n' );
+document.write( '<div><a class=\"list-group-item list-group-item-action\" href=\"http://krunker.io\" target=\"1560815412c3\">krunker.io</a>\n' );
+document.write( '</div>\n' );
+document.write( '<div class=\"clear\"></div>\n' );
+document.write( '</div>\n' );
+document.write( '</br>\n' );
+document.write( '<textarea class=\"form-control\" name=\"id\" rows=\"10\">krunker.io\n' );
+document.write( 'krunker.io\n' );
+document.write( 'krunker.io' );
 
